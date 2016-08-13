5741a7f1448c105098955a19f39791124ea0b050
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-08-13 22:05:19 +0900

    Add FullScratchRadioGroup

---
+++ app/src/main/java/com/os/operando/makeradiogroup/widget/FullScratchRadioGroup.java
@@ -0,0 +1,33 @@
+package com.os.operando.makeradiogroup.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.IdRes;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+public class FullScratchRadioGroup extends LinearLayout {
+
+    private OnCheckedChangeListener onCheckedChangeListener;
+
+    public FullScratchRadioGroup(Context context) {
+        super(context);
+    }
+
+    public FullScratchRadioGroup(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public FullScratchRadioGroup(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public FullScratchRadioGroup(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
