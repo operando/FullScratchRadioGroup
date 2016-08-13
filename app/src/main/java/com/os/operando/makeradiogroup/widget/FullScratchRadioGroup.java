f4cf1f5062c3effe8a2e7132a7ed40298f23e3ae
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-08-13 22:06:33 +0900

    Add LayoutParams

--- app/src/main/java/com/os/operando/makeradiogroup/widget/FullScratchRadioGroup.java
+++ app/src/main/java/com/os/operando/makeradiogroup/widget/FullScratchRadioGroup.java
@@ -34,6 +34,50 @@ public class FullScratchRadioGroup extends LinearLayout {
         this.onCheckedChangeListener = onCheckedChangeListener;
     }
 
+    public static class LayoutParams extends LinearLayout.LayoutParams {
+
+        public LayoutParams(Context c, AttributeSet attrs) {
+            super(c, attrs);
+        }
+
+        public LayoutParams(int width, int height) {
+            super(width, height);
+        }
+
+        public LayoutParams(int width, int height, float weight) {
+            super(width, height, weight);
+        }
+
+        public LayoutParams(ViewGroup.LayoutParams p) {
+            super(p);
+        }
+
+        public LayoutParams(MarginLayoutParams source) {
+            super(source);
+        }
+
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        public LayoutParams(LinearLayout.LayoutParams source) {
+            super(source);
+        }
+
+        @Override
+        protected void setBaseAttributes(TypedArray a, int widthAttr, int heightAttr) {
+            super.setBaseAttributes(a, widthAttr, heightAttr);
+
+            if (a.hasValue(widthAttr)) {
+                width = a.getLayoutDimension(widthAttr, "layout_width");
+            } else {
+                width = WRAP_CONTENT;
+            }
+
+            if (a.hasValue(heightAttr)) {
+                height = a.getLayoutDimension(heightAttr, "layout_height");
+            } else {
+                height = WRAP_CONTENT;
+            }
+        }
+    }
 
     public interface OnCheckedChangeListener {
         void onCheckedChanged(FullScratchRadioGroup fullScratchRadioGroup, @IdRes int checkedId);
