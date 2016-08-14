5664e52fcd5f7ad45ce987e635b28e65c7616c6a
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2016-08-14 19:18:21 +0900

    sample

--- app/src/main/java/com/os/operando/makeradiogroup/MainActivity.java
+++ app/src/main/java/com/os/operando/makeradiogroup/MainActivity.java
@@ -1,13 +1,27 @@
 package com.os.operando.makeradiogroup;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.os.operando.makeradiogroup.widget.FullScratchRadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FullScratchRadioGroup radioGroup = (FullScratchRadioGroup) findViewById(R.id.radio_group);
+        radioGroup.setOnCheckedChangeListener(new FullScratchRadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(FullScratchRadioGroup fullScratchRadioGroup, @IdRes int checkedId) {
+                Log.d(TAG, "onCheckedChanged : checkedId = " + checkedId);
+            }
+        });
     }
 }
